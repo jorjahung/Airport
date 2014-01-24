aca2822198d33ca921c1db6ae1ed7713c861f6be
Jorja Hung <jorjahung@gmail.com>
2014-01-24 14:43:11 +0000

    Added the option of adding capacity

--- spec/grand_finale_spec.rb
+++ spec/grand_finale_spec.rb
@@ -5,6 +5,7 @@ require_relative "../lib/plane"
 describe "The Grand Finale" do 
 let(:airport) { Airport.new }
 let(:planes)  { Array.new(6) { Plane.new } }
+let(:tinyairport) { Airport.new(:capacity=> 5)}
 
 	it "should land all planes" do
 		planes.each do |plane|
@@ -27,5 +28,16 @@ let(:planes)  { Array.new(6) { Plane.new } }
 		end
 		expect(airport.plane_counter).to eq(0)
 	end
+
+	it "first plane should take off when airport is full" do
+		planes.each do |plane|
+			plane.landing
+			airport.land(plane)
+			expect(plane).to_not be_flying
+			expect(plane.flight_status).to eq("On ground")
+		end
+		expect(airport.plane_counter).to eq(5)
+	end
+
 
 end
