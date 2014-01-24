2dfdc030c4ceb4571f466561d0a367d3532da41d
Jorja Hung <jorjahung@gmail.com>
2014-01-24 15:54:53 +0000

    Final answer(?)

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,16 +4,16 @@ require_relative "../lib/plane"
 describe Plane do 
 	let(:plane) { Plane.new }
 
-	it "flight status should be 'flying' when created" do
-		expect(plane.flight_status).to eq("In flight")
+	it "flight status should be flying when created" do
+		expect(plane).to be_flying
 	end
 
-	it "can take off and status must be flying" do
+	it "can take off and status must be 'In flight'" do
 		plane.takeoff
 		expect(plane.flight_status).to eq("In flight")
 	end
 
-	it "can land and status must be 'Landed'" do
+	it "can land and status must be 'On ground'" do
 		plane.landing
 		expect(plane.flight_status).to eq("On ground")
 	end
