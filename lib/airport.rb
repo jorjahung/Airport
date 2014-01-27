300958a0ca96d18edfb6ac0ff0ebab2a3fcbea1b
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2014-01-27 15:37:30 +0000

    feedback

--- lib/airport.rb
+++ lib/airport.rb
@@ -23,6 +23,8 @@ class Airport
 	end
 
 	def land(plane)
+		# if you want to throw an exception, do it here, not in the method, e.g.
+		# raise "Bad weather" if clear_to_fly?
 		check_weather
 		raise "Can't land because airport is full" if full?
 		plane.landing
@@ -36,7 +38,10 @@ class Airport
 	end
 
 	def check_weather
+		# It looks like you're using Kernel.rand here only because it's convenient
+		# to stub it in the test but there's no need for this.
 		weather = Kernel.rand(50)
+		# As mentioned above, it's better to raise the exception elsewhere
 		raise "Can't fly due to weather conditions" if weather == 50 
 	end
 
